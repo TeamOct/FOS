109de9d7aff6acecdc057072b933276368562af7
TheSlaylord <abevnyaev@gmail.com>
2020-12-07 15:36:14 +0300

    извините, поляр сломал планеты

--- scripts/planets.js
+++ scripts/planets.js
@@ -18,9 +18,9 @@ mars.accessible = false;
 mars.hasAtmosphere = false;
 
 const polaris = new JavaAdapter(Planet, {}, "polaris", Planets.sun, 4, 0.6);
-mars.meshLoader = () => new SunMesh(polaris, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("7b68ee"), Color.valueOf("6a5acd"), Color.valueOf("4169e1"));
-mars.accessible = false;
-mars.hasAtmosphere = false;
+polaris.meshLoader = () => new SunMesh(polaris, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("7b68ee"), Color.valueOf("6a5acd"), Color.valueOf("4169e1"));
+polaris.accessible = false;
+polaris.hasAtmosphere = false;
 
 const moon = new JavaAdapter(Planet, {}, "moon", Planets.serpulo, 4, 0.7);
 moon.meshLoader = () => new SunMesh(moon, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("847982"), Color.valueOf("939393"), Color.valueOf("837881"));
@@ -28,11 +28,11 @@ moon.accessible = false;
 moon.hasAtmosphere = false;
 
 const saylot = new JavaAdapter(Planet, {}, "saylot", Planets.polaris, 4, 0.7);
-moon.meshLoader = () => new SunMesh(moon, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("000000"), Color.valueOf("ff9900"), Color.valueOf("323332"));
-moon.accessible = false;
-moon.hasAtmosphere = false;
+saylot.meshLoader = () => new SunMesh(saylot, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("000000"), Color.valueOf("ff9900"), Color.valueOf("323332"));
+saylot.accessible = false;
+saylot.hasAtmosphere = false;
 
 const ko = new JavaAdapter(Planet, {}, "ko", Planets.polaris, 4, 0.7);
-moon.meshLoader = () => new SunMesh(ko, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("000000"), Color.valueOf("ff9900"), Color.valueOf("323332"));
-moon.accessible = false;
-moon.hasAtmosphere = false;
+ko.meshLoader = () => new SunMesh(ko, 4, 5, 0.3, 1.7, 1.2, 1, 1.5, Color.valueOf("000000"), Color.valueOf("ff9900"), Color.valueOf("323332"));
+ko.accessible = false;
+ko.hasAtmosphere = false;
