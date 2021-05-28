a8cadf61700ce6b33fc2ec7b347cd009c4e8e841
TheSlaylord <abevnyaev@gmail.com>
2021-05-28 20:25:01 +0300

    Moon starting sector, Planets in tech tree

--- scripts/sectorpresets.js
+++ scripts/sectorpresets.js
@@ -1,2 +1,9 @@
+//region planets
+const moon = Vars.content.getByName(ContentType.planet, "fos-moon");
+//endregion
+
 const siloTerminal = new SectorPreset("siloTerminal", Planets.serpulo, 95);
 siloTerminal.difficulty = 8;
+
+const landingSiteMoon = new SectorPreset("landingSiteMoon", moon, 221);
+landingSiteMoon.difficulty = 1;
--- scripts/techtree.js
+++ scripts/techtree.js
@@ -3,21 +3,28 @@ const newNode = (parent, content, req, objectives) => {
   const node = new TechTree.TechNode(parnode, content, req != null ? req : content.researchRequirements());
   var used = new ObjectSet();
   
-  if ((objectives != null) && !(content instanceof SectorPreset)){
+  if (objectives != null){
     node.objectives.addAll(objectives);
   }
 }
 
 const rocketSilo = Vars.content.getByName(ContentType.block, "fos-rocket-silo");
-const siloTerminal = Vars.content.getByName(ContentType.sector, "fos-siloTerminal");
 
-newNode(Blocks.launchPad,
-  rocketSilo,
-  null,
-  Seq.with(new Objectives.SectorComplete(siloTerminal))
-);
-newNode(SectorPresets.nuclearComplex,
-  siloTerminal,
-  null,
-  Seq.with(new Objectives.SectorComplete(SectorPresets.nuclearComplex), new Objectives.Research(Blocks.launchPad), new Objectives.Research(Blocks.interplanetaryAccelerator))
-);
+const siloTerminal = Vars.content.getByName(ContentType.sector, "fos-siloTerminal");
+const lsMoon = Vars.content.getByName(ContentType.sector, "fos-landingSiteMoon");
+
+const moon = Vars.content.getByName(ContentType.planet, "fos-moon");
+
+//region planets branch
+newNode(Blocks.coreShard, Planets.serpulo, null, null);
+Planets.serpulo.alwaysUnlocked = true;
+newNode(Planets.serpulo, moon, null, Seq.with(new Objectives.Research(Blocks.interplanetaryAccelerator)));
+//endregion
+
+//region sectors
+newNode(SectorPresets.nuclearComplex, siloTerminal, null, Seq.with(new Objectives.SectorComplete(SectorPresets.nuclearComplex), new Objectives.Research(Blocks.launchPad), new Objectives.Research(Blocks.interplanetaryAccelerator)));
+
+newNode(moon, lsMoon, null, null);
+//endregion
+
+newNode(Blocks.launchPad, rocketSilo, null, Seq.with(new Objectives.SectorComplete(siloTerminal)));
