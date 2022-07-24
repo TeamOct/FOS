604d8419a6eb0d84086de7538226c0c0556b0267
TheSlaylord <abevnyaev@gmail.com>
2022-07-24 22:24:12 +0300

    The First Steps

--- scripts/planets/mars-waves.js
+++
@@ -1,8 +0,0 @@
-// SCRAPPED FOR NOW, AND HAS NO PURPOSE!!!
-
-const species = [
-  [UnitTypes.reign, UnitTypes.reign, UnitTypes.reign, UnitTypes.reign, UnitTypes.reign],
-  [UnitTypes.nova, UnitTypes.pulsar, UnitTypes.quasar, UnitTypes.vela, UnitTypes.corvus],
-  [UnitTypes.crawler, UnitTypes.atrax, UnitTypes.spiroct, UnitTypes.arkyid, UnitTypes.toxopid],
-  [UnitTypes.flare, UnitTypes.horizon, UnitTypes.zenith, Mathf.chance(0.5) ? UnitTypes.quad : UnitTypes.antumbra, UnitTypes.eclipse]
-  ];
