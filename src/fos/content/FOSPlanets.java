ec6109f01d9daeb05ec7f67e41784f2ef5d75814
TheSlaylord <abevnyaev@gmail.com>
2022-09-14 16:25:00 +0300

    Update FOSPlanets.java

--- src/fos/content/FOSPlanets.java
+++ src/fos/content/FOSPlanets.java
@@ -30,7 +30,7 @@ public class FOSPlanets {
             startSector = 9;
             generator = new LuminaPlanetGenerator();
             minZoom = 0.8f;
-            camRadius += 1f;
+            camRadius += 0.4f;
             cloudMeshLoader = () -> new HexSkyMesh(this, 7, 1.1f, 0.15f, 7, Color.valueOf("b0dcb76d"), 2, 0.5f, 1f, 0.38f);
             ruleSetter = r -> {
                 r.fog = true;
