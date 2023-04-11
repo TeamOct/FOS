9de96a7682c393fec0ea88284b4ca00378842e5d
nekit508 <lastp508@gmail.com>
2023-04-11 20:41:02 +0300

    damage display fix

--- src/fos/ui/DamageDisplay.java
+++ src/fos/ui/DamageDisplay.java
@@ -7,26 +7,15 @@ import arc.graphics.g2d.Draw;
 import arc.graphics.g2d.GlyphLayout;
 import arc.graphics.g2d.TextureRegion;
 import arc.math.Mathf;
-import arc.math.geom.Vec2;
-import arc.scene.actions.Actions;
-import arc.scene.event.Touchable;
 import arc.scene.style.TextureRegionDrawable;
-import arc.scene.ui.layout.Table;
 import arc.struct.ObjectFloatMap;
 import arc.struct.Seq;
 import arc.util.*;
-import mindustry.Vars;
-import mindustry.content.StatusEffects;
-import mindustry.core.GameState;
 import mindustry.entities.Effect;
-import mindustry.game.EventType;
 import mindustry.gen.*;
 import mindustry.graphics.Layer;
 import mindustry.graphics.Pal;
 import mindustry.ui.Fonts;
-import mindustry.ui.Styles;
-import mindustry.world.Block;
-import mindustry.world.Build;
 
 import static mindustry.Vars.state;
 import static mindustry.Vars.world;
@@ -36,9 +25,12 @@ public class DamageDisplay {
     /** Entities health check frequency in ticks (1 second = 60 ticks) **/
     public float updateFrequency = 30f;
     /** Stores previous entity health values. */
-    public static final ObjectFloatMap<Healthc> entities = new ObjectFloatMap<>();
+    public final ObjectFloatMap<Healthc> entitiesHealth = new ObjectFloatMap<>();
     /** Stores previous entity health values. */
-    public static final ObjectFloatMap<Building> nonUpdateBuildings = new ObjectFloatMap<>();
+    public final ObjectFloatMap<Building> nonUpdateBuildingsHealth = new ObjectFloatMap<>();
+    /** Stores entities **/
+    public final Seq<Healthc> entities = new Seq<>();
+    public final Seq<Building> nonUpdateBuildings = new Seq<>();
 
     public static Effect damageShowEffect = new Effect(60f, 80f, (e) -> {
         HealthInfo data = e.data();
@@ -90,51 +82,67 @@ public class DamageDisplay {
                     if (progress >= updateFrequency) {
                         progress %= updateFrequency;
 
-                        // for updatable entities
-                        Groups.all.each(e -> {
-                            if (e instanceof Healthc he) {
-                                if (!e.isAdded()) {
-                                    entities.remove(he, 0);
-                                    return;
-                                }
-
-                                if (entities.containsKey(he)) {
-                                    float delta = entities.get(he, 0) - he.health();
-                                    if (delta != 0)
-                                        damageShowEffect.at(he.x(), he.y(), 0, new HealthInfo(
-                                                delta > 0 ? 1 : 2, Math.abs(delta)));
-                                }
-
-                                entities.put(he, he.health());
-                            }
-                        });
-
-                        // for non-update buildings
-                        world.tiles.eachTile(tile -> {
-                            Building building = tile.build;
-                            if (building == null) return;
-                            if (!building.isAdded()) {
-                                if (nonUpdateBuildings.containsKey(building)) {
-                                    float delta = nonUpdateBuildings.get(building, 0) - building.health();
-                                    if (delta != 0)
-                                        damageShowEffect.at(building.x(), building.y(), 0, new HealthInfo(
-                                                delta > 0 ? 1 : 2, Math.abs(delta)));
-                                }
-
-                                nonUpdateBuildings.put(building, building.health);
-                            }
-                        });
-
-                        // do not delete entries inside for-loop
-                        forRemove.clear();
-                        nonUpdateBuildings.each(e -> {
-                            if (e.key.tile.build != e.key) forRemove.add(e.key);
-                        });
-                        forRemove.each(b -> nonUpdateBuildings.remove(b, 0));
+                        update();
                     }
                 }
             }
         });
+    }
+
+    void update() {
+        // add all in-world entities
+        Groups.all.each(e -> {
+            if (e instanceof Healthc he)
+            if (!entities.contains(he))
+                entities.add(he);
+        });
+        world.tiles.eachTile(tile -> {
+            Building building = tile.build;
+            if (building == null) return;
+            if (!nonUpdateBuildings.contains(building))
+                nonUpdateBuildings.add(building);
+        });
+
+        // for updatable entitiesHealth
+        entities.each(he -> {
+            he.clampHealth();
+            if (entitiesHealth.containsKey(he)) {
+                float delta = entitiesHealth.get(he, 0) - he.health();
+                if (delta != 0)
+                    damageShowEffect.at(he.x(), he.y(), 0, new HealthInfo(
+                            delta > 0 ? 1 : 2, Math.abs(delta)));
+            }
+
+            if (!he.isAdded()) {
+                entitiesHealth.remove(he, 0);
+                entities.remove(he);
+                return;
+            }
+
+            entitiesHealth.put(he, he.health());
+        });
+
+        // for non-update buildings
+        nonUpdateBuildings.each(b -> {
+            b.clampHealth();
+            if (!b.isAdded()) {
+                if (nonUpdateBuildingsHealth.containsKey(b)) {
+                    float delta = nonUpdateBuildingsHealth.get(b, 0) - b.health();
+                    if (delta != 0)
+                        damageShowEffect.at(b.x(), b.y(), 0, new HealthInfo(
+                                delta > 0 ? 1 : 2, Math.abs(delta)));
+                }
+
+                nonUpdateBuildingsHealth.put(b, b.health);
+            }
+        });
+
+        // do not delete entries inside for-loop
+        forRemove.clear();
+        nonUpdateBuildingsHealth.each(e -> {
+            if (e.key.tile.build != e.key) forRemove.add(e.key);
+        });
+        forRemove.each(b -> nonUpdateBuildingsHealth.remove(b, 0));
     }
 
     static class HealthInfo {
