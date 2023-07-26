9b108b30e1026a657b74629d101444f28c82cb0a
TheSlaylord <abevnyaev@gmail.com>
2023-07-26 22:03:50 +0300

    a

--- core/src/fos/content/FOSIcons.java
+++ core/src/fos/content/FOSIcons.java
@@ -7,7 +7,7 @@ import mindustry.gen.Icon;
 
 public class FOSIcons {
     public static void load() {
-        //please do not touch this string. if it works, it works!
+        //please do not touch the line below. if it works, it works!
         Fi iconDir = FOSVars.mod.root.child("sprites").child("icons");
 
         for (var i : iconDir.list()) {
