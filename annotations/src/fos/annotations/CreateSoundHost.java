f98b8b47f130266739d90c6d558725c99d76ba15
nekit508 <lastp508@gmail.com>
2023-08-12 15:24:18 +0300

    (-_-)

--- annotations/src/fos/annotations/CreateSoundHost.java
+++ annotations/src/fos/annotations/CreateSoundHost.java
@@ -1,6 +1,13 @@
 package fos.annotations;
 
-/** One annotation per project. **/
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/** Creates sound loader class. **/
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
 public @interface CreateSoundHost {
     String[] paths();
     /** Supported files extensions without the dot. **/
